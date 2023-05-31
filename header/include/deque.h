25264980e8755cbe7dc1e601044f30deb75f4dee
Jigchu <111167590+Jigchu@users.noreply.github.com>
2023-05-31 11:15:29 +0800

    Added semicolon

--- header/include/deque.h
+++ header/include/deque.h
@@ -9,5 +9,5 @@ void *pop(deque *stack);
 int queue(deque *queue, void *value);
 void *dequeue(deque *queue);
 void *get_element(deque *dequeue, long index);
-void *get_count(deque *dequeue)
+void *get_count(deque *dequeue);
 void **search(deque *dequeue, void *value, bool same(const void *, const void *));
